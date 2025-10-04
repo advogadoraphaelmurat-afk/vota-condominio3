2bd279e4739b4ed1193535d2676f7c2b0b64f56f
Raphael Murat <advogadoraphaelmurat@gmail.com>
2025-10-04 11:43:29 -0300

    Fix test page

--- app/teste-db/page.tsx
+++ app/teste-db/page.tsx
@@ -1,7 +1,14 @@
-export default function TestePage() {
+async function TestePage() {
+  // Exemplo buscando dados de uma API
+  const response = await fetch('https://api.exemplo.com/dados');
+  const data = await response.json();
+
   return (
-  <>
-    <div>Teste de Conexão</div>
-    <pre>{JSON.stringify(data, null, 2)}</pre>
-  </>
-)
+    <>
+      <div>Teste de Conexão</div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
+}
+
+export default TestePage;
